a017cc446f1dce0fcc0ef5c66b5d3db10c0eb823
Ilyos <ilyosshaydullaye212@gmail.com>
2023-09-05 16:37:18 +0500

    Start

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict'
+//Browser 3ga bo'linadi: 
+// 1)DOM; 2)BOM; 3)JS
+// const firstElement=document.body.children[0];
+// const secondElement=document.body.style.color="green"
+// const bodyElement=document.body;
+// console.log(bodyElement.children);
+// console.log(bodyElement.childNodes);
+// Children vs ChildNodesni farqi: Birinchisi HTML Collection qaytaradi; Ikkinchisi NodeList qaytaradi.
+// HTML Collection aniq bir elementlar qatoridan joy olgan aniq bir elementlarni targeting qiladi, NodeList esa aniq bir elementlarga bog'liq bo'lgan yashirin elementlarni chiqarish ham invite qiladi 
+
+// console.log(firstElement.parentElement);
+// console.log(firstElement.lastElementChild);
+// console.log(firstElement.lastChild);
+// console.log(firstElement.nextSibling);
+// console.log(firstElement.nextElementSibling);
+// console.log(firstElement.previousElementSibling);
